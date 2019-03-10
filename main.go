5cb7a94a5691ccdb92615709f24f995d8d759942
ikerrr <729720555@qq.com>
2019-03-10 16:13:20 +0800

    创建区块连结构及创世区块

--- main.go
+++ main.go
@@ -19,16 +19,37 @@ func NewBlock(preHash []byte,data string)*Block{
 	block.SetHash()
 	return &block
 }
-//生成哈希
+//3.生成哈希
 func (block *Block)SetHash(){
 	blockInfo:=append(block.PreHash,block.Data...)
 	hash:=sha256.Sum256(blockInfo)
 	block.Hash=hash[:]
 }
 
+//4.引入区块连
+type BlockChain struct {
+	blocks []*Block
+}
+//5.定义一个区块连
+func NewBlockChain()*BlockChain  {
+	genesisBlock:=GenesisBlock()
+	 return &BlockChain{
+	 	blocks:[]*Block{genesisBlock},
+	 }
+}
+//6.定义创世区块
+func GenesisBlock()*Block{
+	return NewBlock([]byte{},"创世区块，真牛鼻！")
+}
+
 func main(){
-	block :=NewBlock([]byte{},"老师给班长红钻一个BTC")
-	fmt.Printf("前去块哈希：%x\n",block.PreHash)
-	fmt.Printf("当前区块哈希：%x\n",block.Hash)
-	fmt.Printf("区块哈希值：%s\n",block.Data)
+     bc:=NewBlockChain()
+     for i,block:=range bc.blocks{
+     	 fmt.Printf("=======当前区块高度:%d======\n",i)
+		 fmt.Printf("前去块哈希：%x\n",block.PreHash)
+		 fmt.Printf("当前区块哈希：%x\n",block.Hash)
+		 fmt.Printf("区块哈希值：%s\n",block.Data)
+	 }
+
+
 }
