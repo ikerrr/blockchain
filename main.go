b68db33f2f2aeb613c38216d46002c2aa275f91b
ikerrr <729720555@qq.com>
2019-03-10 16:40:07 +0800

    添加土块AddBlock

--- main.go
+++ main.go
@@ -41,14 +41,25 @@ func NewBlockChain()*BlockChain  {
 func GenesisBlock()*Block{
 	return NewBlock([]byte{},"创世区块，真牛鼻！")
 }
+//7.天津爱区块实现
+func (bc *BlockChain)AddBlock(data string){
+	lastBlock:=bc.blocks[len(bc.blocks)-1]
+	prevHash:=lastBlock.Hash
+
+	block:=NewBlock(prevHash,data)
+	bc.blocks=append(bc.blocks,block)
+}
 
 func main(){
      bc:=NewBlockChain()
+     bc.AddBlock("班长向班花转了50个BTC")
+     bc.AddBlock("班长又向班花转了50个BTC，向干嘛亚")
+     bc.AddBlock("呵呵哒")
      for i,block:=range bc.blocks{
-     	 fmt.Printf("=======当前区块高度:%d======\n",i)
+     	 fmt.Printf("==========当前区块高度:%d==========\n",i)
 		 fmt.Printf("前去块哈希：%x\n",block.PreHash)
 		 fmt.Printf("当前区块哈希：%x\n",block.Hash)
-		 fmt.Printf("区块哈希值：%s\n",block.Data)
+		 fmt.Printf("区快数据：%s\n",block.Data)
 	 }
 
 
